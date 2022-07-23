f8cbdb041b06787c94b6a77a666a9b841b921741
seshan-b <seshan.b@gmail.com>
2022-07-23 14:41:54 +1200

    Get input and print name

---
+++ print_name.py
@@ -0,0 +1,11 @@
+# Get user input
+user_input = input("What is your name? ")
+
+
+# Get 1st and last name
+first_name = user_input.split()[0]
+last_name = user_input.split()[1]
+
+
+# print
+print(f"My name is {first_name.title()} {last_name.title()}")
