5b0aa34e5b23d2a3b73ca0134f0ec9ca0eef0912
seshan-b <seshan.b@gmail.com>
2022-07-25 14:50:09 +1200

    Get odd or even number based on input

---
+++ odd_even_numbers.py
@@ -0,0 +1,17 @@
+
+
+# Get user input
+number_input = int(input("Which number do you want to check? "))
+
+# Assign input to variable
+number = number_input
+
+
+# Do calculation 
+mod_number =  number % 2
+
+# Check and output number.
+if (mod_number >= 1) :
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
