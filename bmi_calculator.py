2901e3ba075c89c7e7fae91785dd800f21265cbf
seshan-b <seshan.b@gmail.com>
2022-07-26 14:56:52 +1200

    Get BMI based on weight and height

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -4,14 +4,12 @@
 input_height = input("enter your height in m: ")
 input_weight = input("enter your weight in kg: ")
 
-
 # Assign variable and type cast
 height = float(input_height) 
 weight = int(input_weight)
 
 # Calculate
 bmi = weight / height ** 2
-
 
 # Print
 print(f" Your BMI is {str(round(bmi))}")
