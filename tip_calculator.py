59056dde0f49b7015f856455729539823d5775fc
seshan-b <seshan.b@gmail.com>
2022-07-27 15:24:22 +1200

    Tip Calculator

--- tip_calculator.py
+++ tip_calculator.py
@@ -5,3 +5,25 @@
 
 
 # Round the result to 2 decimals 
+
+
+# Print heading
+print("Print Calculator")
+
+bill = float(input("What was the total bill? $"))
+
+tip = int(input("How much would you like to tip 10, 12, 15? "))
+
+people = int(input("How many people would you split it with"))
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+
+bill_per_person = total_bill / people
+
+final_amount = round(bill_per_person)
+
+final_amount = "{:.2f}".format(bill_per_person)
+
+print(f"Your bill is ${final_amount}")
