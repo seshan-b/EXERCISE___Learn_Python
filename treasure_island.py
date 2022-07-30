31da227e6fbd2d9dc4994603c9eee2cd981e827a
seshan-b <seshan.b@gmail.com>
2022-07-30 16:20:04 +1200

    Treasure Island

--- treasure_island.py
+++ treasure_island.py
@@ -25,3 +25,8 @@ print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
 choice1 = input("You're at a crossroad, where do you want to go? Type \"left\" or \"right\". \n").lower()
+
+if choice1 == "left":
+      choice2 = input("You have come to a lake. There is a island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
+else: 
+    print("You fell into a hole. GAME OVER!")
