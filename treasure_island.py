6cc00b64b97813d208f7a742bf9c30307d266abf
seshan-b <seshan.b@gmail.com>
2022-07-31 16:37:09 +1200

    Treasure Island

--- treasure_island.py
+++ treasure_island.py
@@ -30,6 +30,12 @@ if choice1 == "left":
     choice2 = input("You have come to a lake. There is a island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across.\n").lower()
     if choice2 == "wait":
         choice3 = input("You have arrived at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?\n").lower()
+        if choice3 == "red" or choice3 == "blue":
+            print("GAME OVER!")
+        elif choice3 == "yellow":
+            print("You WIN!")
+        else: 
+            print("GAME OVER!")   
     else:
         print("You are attacked by a angry trout Game Over!")   
 else: 
