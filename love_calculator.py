10cb3c9b57604fc6d52d66b111e6071e5d69ff23
seshan-b <seshan.b@gmail.com>
2022-08-01 16:44:14 +1200

    Love Calculator

--- love_calculator.py
+++ love_calculator.py
@@ -4,3 +4,7 @@ name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
 combine_string = name1 + name2
+
+lowercase_combined_string = combine_string.lower()
+
+t = lowercase_combined_string.count("t")
