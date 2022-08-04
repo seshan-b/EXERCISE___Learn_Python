37bd6cb189dca83f0aae26dc4ac21e4d7d49d085
seshan-b <seshan.b@gmail.com>
2022-08-04 16:50:46 +1200

    Love Calculator

--- love_calculator.py
+++ love_calculator.py
@@ -18,3 +18,5 @@ l = lowercase_combined_string.count("l")
 o = lowercase_combined_string.count("o")
 v = lowercase_combined_string.count("v")
 e = lowercase_combined_string.count("e")
+
+love = l + o + v + e
