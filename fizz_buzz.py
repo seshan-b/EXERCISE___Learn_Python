2aa2b8bf7a4bb1020623d26922b7027eb84e2f99
seshan-b <seshan.b@gmail.com>
2022-08-06 12:43:03 +1200

    Fizz Buzz

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -4,7 +4,7 @@ for number in range(0, 101):
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
     elif number % 3 == 0:
-        
+        print("Fizz")       
     elif number % 5 == 0:
-        
+                
     else:
