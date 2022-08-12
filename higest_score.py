9f25529f0d106d604c89bf7c62a7188d5b18ae0c
seshan-b <seshan.b@gmail.com>
2022-08-12 12:50:47 +1200

    Higest Score

--- higest_score.py
+++ higest_score.py
@@ -6,3 +6,6 @@ student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
     student_scores[n] = int(student_scores[n])
 print(student_scores)
+
+
+# A simple way to write this code above.
