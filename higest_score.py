cc2c0e81a81afbc343c8b3b965bbbdd9f94eca8b
seshan-b <seshan.b@gmail.com>
2022-08-13 12:56:15 +1200

    Higest Score

--- higest_score.py
+++ higest_score.py
@@ -11,9 +11,9 @@ print(student_scores)
 # A simple way to write this code above.
 # max_score = max(student_scores)
 # print(f"The highest score in the class is: {max_score}")
-
+# --------------------------------------------------------------
+# Doing the above code with an Loop
 highest_score = 0
-
 for score in student_scores:
     if score > highest_score:
           highest_score = score 
