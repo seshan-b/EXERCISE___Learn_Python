42de712572477e44aa29f95ef334694b917ffaa0
seshan-b <seshan.b@gmail.com>
2022-08-16 13:04:51 +1200

    Average Height

--- average_height.py
+++ average_height.py
@@ -3,6 +3,10 @@
 # Get input from user
 student_heights = input("Input a list of student heights ").split()
 
+# Loop through the input heights
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 print(student_heights)
+
+
+# Sum up all the heights
