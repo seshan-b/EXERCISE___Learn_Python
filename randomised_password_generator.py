84626b434fd970f5c5d77f5370f9bd85cb248429
seshan-b <seshan.b@gmail.com>
2022-08-27 13:48:41 +1200

    Ransomised Password Generator

--- randomised_password_generator.py
+++ randomised_password_generator.py
@@ -14,3 +14,5 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
+
+user_letters= int(input("How many letters would you like in your password?\n")) 
