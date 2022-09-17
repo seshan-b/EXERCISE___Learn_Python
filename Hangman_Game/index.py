8fac12f3fd0a6fca1f60eb572e15048611155311
seshan-b <seshan.b@gmail.com>
2022-09-17 17:35:17 +1200

    Hangman Game - Sudo Code

--- Hangman_Game/index.py
+++ Hangman_Game/index.py
@@ -6,4 +6,9 @@
 
 
 
-#TODO-2: - Loop through each position in the chosen_word;
+#TODO-2: - Loop through each position in the chosen_word;gp
+
+
+
+
+#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
