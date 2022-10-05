fada422bb9dc0a574b4da93d5ca84b3bfff80246
seshan-b <seshan.b@gmail.com>
2022-10-05 16:02:39 +1300

    Caeser Cipher setup

--- Caeser_Cipher/index.py
+++ Caeser_Cipher/index.py
@@ -8,3 +8,8 @@ print(start_logo)
 
 # The Letter to scramble
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+def caeser(start_text, shift_amount, cipher_direction):
+    
