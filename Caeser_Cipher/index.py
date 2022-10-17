53846c674a5724e099cfbad323f7e8bb3e10d0e0
seshan-b <seshan.b@gmail.com>
2022-10-17 17:26:23 +1300

    Caeser Cipher

--- Caeser_Cipher/index.py
+++ Caeser_Cipher/index.py
@@ -7,7 +7,8 @@ print(start_logo)
 
 
 # The Letter to scramble
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+            'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 # Function
@@ -18,11 +19,11 @@ def caeser(start_text, shift_amount, cipher_direction):
             position = alphabet.index(char)
             # Check to see if encoded or decoded
             if cipher_direction == "encode":
-                new_position = position + shift_amount 
+                new_position = position + shift_amount
                 end_text += alphabet[new_position]
             elif cipher_direction == "decode":
-                 new_position = position - shift_amount
-                 end_text += alphabet[new_position]
+                new_position = position - shift_amount
+                end_text += alphabet[new_position]
         else:
             end_text += char
         print(f"The {cipher_direction} is  {end_text}")
@@ -30,20 +31,22 @@ def caeser(start_text, shift_amount, cipher_direction):
 
 should_continue = True
 while should_continue:
-    
+
     # Ask questions
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
+
+    # The Modulus will get the remainder of each index shift position. 
     
     shift = shift % 26
-    
+
     # Calling the function
     caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
-    
-    
+
     # Ask to continue
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
-    
+    restart = input(
+        "Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+
     should_continue = False
     print("Goodbye")
