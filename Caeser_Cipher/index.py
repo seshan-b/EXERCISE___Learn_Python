3a16add5dad0507bc8bfce40c40b756d6fc1c7ef
seshan-b <seshan.b@gmail.com>
2022-10-17 17:19:03 +1300

    Caeser Cipher

--- Caeser_Cipher/index.py
+++ Caeser_Cipher/index.py
@@ -29,13 +29,15 @@ def caeser(start_text, shift_amount, cipher_direction):
 
 should_continue = True
 while should_continue:
+    
+    
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     
     shift = shift % 26
     
-    
+    # Calling the function
     caeser(start_text=text, shift_amount=shift, cipher_direction=direction)
     
     restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
