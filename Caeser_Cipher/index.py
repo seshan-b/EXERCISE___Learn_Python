6e2357fe53edc9c386166af2789fb7803a438f77
seshan-b <seshan.b@gmail.com>
2022-10-17 17:16:10 +1300

    Caeser Cipher

--- Caeser_Cipher/index.py
+++ Caeser_Cipher/index.py
@@ -31,3 +31,6 @@ should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    
+    shift = shift % 26
