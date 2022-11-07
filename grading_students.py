57e5d323764e533421c028e77948fb1e44a59cc2
seshan-b <seshan.b@gmail.com>
2022-11-07 13:15:23 +1300

    Clean up

--- grading_students.py
+++ grading_students.py
@@ -22,7 +22,7 @@ for student in student_scores:
         student_grades[student] = "Acceptable"
     else:
         student_grades[student] = "Fail"
-        
+
 
 # 🚨 Don't change the code below 👇
 print(student_grades)
--- higest_score.py
+++ higest_score.py
@@ -21,5 +21,5 @@ print(student_scores)
 highest_score = 0
 for score in student_scores:
     if score > highest_score:
-          highest_score = score 
+        highest_score = score 
     print(highest_score)
