8809f4959589a14e8b8a44f7c2913d8697ef018d
seshan-b <seshan.b@gmail.com>
2022-11-28 16:55:18 +1300

    Secret Auction

--- secret_auction.py
+++ secret_auction.py
@@ -29,3 +29,5 @@ bidding_finished = False
 def find_highest_bidder(bidding_record):
     highest_bid = 0
     for bidder in bidding_record:
+        bidding_amount = bidding_record[bidder]
+        
