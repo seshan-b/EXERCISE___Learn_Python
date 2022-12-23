1ebedb0d7cf306ab9bf16da6fd60cef033af6991
seshan-b <seshan.b@gmail.com>
2022-12-23 16:12:42 +1300

    Coding a Calculator

--- Calculators/Simple_Calculator.py
+++ Calculators/Simple_Calculator.py
@@ -52,6 +52,9 @@ def caluclator():
          # Calculate
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
+        
+        
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
      
      
 
