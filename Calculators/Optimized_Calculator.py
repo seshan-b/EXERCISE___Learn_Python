1a18299fc6661ba6f5778e4680fd6ca056d9aaa8
seshan-b <seshan.b@gmail.com>
2022-12-28 16:31:29 +1300

    Coding the optimized version

--- Calculators/Optimized_Calculator.py
+++ Calculators/Optimized_Calculator.py
@@ -1,34 +1,34 @@
 import math
 
-
 def calculator():
     num1 = float(input("What is the first number?"))
+
     while True:
-        input_str = input(
-            "Enter an operation and number (e.g. '* 4' to multiply by 4):")
+        input_str = input("Enter an operation and number (e.g. '* 4' to multiply by 4):")
         try:
-          operation_symbol, num2_str = input_str.split()
-          num2 = float(num2_str)
+            operation_symbol, num2_str = input_str.split()
+            num2 = float(num2_str)
         except ValueError:
-          print("Invalid input. Please try again.")
-        continue
-              if operation_symbol == "+":
+            print("Invalid input. Please try again.")
+            continue
+
+        if operation_symbol == "+":
             num1 += num2
-                    elif operation_symbol == "-":
+        elif operation_symbol == "-":
             num1 -= num2
-                elif operation_symbol == "*":
+        elif operation_symbol == "*":
             num1 *= num2
-                elif operation_symbol == "/":
-            num1 /= num2   elif operation_symbol == "^":
+        elif operation_symbol == "/":
+            num1 /= num2
+        elif operation_symbol == "^":
             num1 = math.pow(num1, num2)
-                  else:
+        else:
             print("Invalid operation. Please try again.")
             continue
-          print(f"{num1} {operation_symbol} {num2} = {num1}")
-          
-          
-           if input(f"Type 'y' to continue calculating with {num1}:") != "y":
+
+        print(f"{num1} {operation_symbol} {num2} = {num1}")
+
+        if input(f"Type 'y' to continue calculating with {num1}:") != "y":
             break
-          
-          
-          calculator()
+
+calculator()
