9adf4469e736d4da51a44b92efaff1c3fb421386
seshan-b <seshan.b@gmail.com>
2022-12-28 16:18:24 +1300

    Coding a Calculator

--- Calculators/Simple_Calculator.py
+++ Calculators/Simple_Calculator.py
@@ -44,26 +44,22 @@ def caluclator():
         print(symbol)
     should_continue = True
 
-
-
-   while should_continue:
+    while should_continue:
         operation_symbol = input("Pick a operation")
         num2 = float(input("What is the next Number?"))
-         # Calculate
+        # Calculate
         calculation_function = operations[operation_symbol]
         answer = calculation_function(num1, num2)
-        
-        
+
         print(f"{num1} {operation_symbol} {num2} = {answer}")
-        
+
         if input(f"Type 'y' to continue calculating with {answer}:") == "y":
-           num1 = answer
+            num1 = answer
         else:
-          should_continue = False
-          
-          caluclator()
-     
-     
+            should_continue = False
+
+            caluclator()
+
 
 # Call it
 caluclator()
