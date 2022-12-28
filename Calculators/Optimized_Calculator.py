f7ee46ec4f9b6f5265daaa32512613237dd5d4a5
seshan-b <seshan.b@gmail.com>
2022-12-28 16:28:35 +1300

    Coding the optimized version

--- Calculators/Optimized_Calculator.py
+++ Calculators/Optimized_Calculator.py
@@ -2,5 +2,6 @@ import math
 
 
 def calculator():
-  num1 = float(input("What is the first number?"))
-  while True:
+    num1 = float(input("What is the first number?"))
+    while True:
+        input_str = input("Enter an operation and number (e.g. '* 4' to multiply by 4):")
