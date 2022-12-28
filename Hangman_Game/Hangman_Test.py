2f90cf2b8bdc6679b9f585136b61cdd05a6faab8
seshan-b <seshan.b@gmail.com>
2022-12-28 18:07:16 +1300

    Testing Hangman code

--- Hangman_Game/Hangman_Test.py
+++ Hangman_Game/Hangman_Test.py
@@ -2,6 +2,7 @@ import main
 
 hangman_game = main
 
+
 def test_hangman_game():
     # Create a mock input function to simulate user input
     def mock_input(prompt):
@@ -9,29 +10,29 @@ def test_hangman_game():
         inputs = ['a', 'b', 'c', 'd', 'e']
         # Return the next input in the sequence
         return inputs.pop(0)
-    
+
     # Create a mock print function to capture the output of the hangman game
     def mock_print(output):
         # Append the output to a list for later assertion
         print_output.append(output)
-    
+
     # Initialize the print_output list to capture the game output
     print_output = []
-    
+
     # Replace the built-in input and print functions with the mock functions
     # This allows us to control the user input and capture the game output for testing
     original_input = __builtins__.input
     original_print = __builtins__.print
     __builtins__.input = mock_input
     __builtins__.print = mock_print
-    
+
     # Run the hangman game
-    hangman_game()
-    
+    caluclator()
+
     # Restore the original input and print functions
     __builtins__.input = original_input
     __builtins__.print = original_print
-    
+
     # Assert that the output is as expected
     assert print_output == [
         # Expected output for the first run of the game
@@ -47,3 +48,4 @@ def test_hangman_game():
     ]
 
 
+test_hangman_game()
