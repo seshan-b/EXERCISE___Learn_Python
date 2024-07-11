cb9254d112d9166a4d2c2f160cc0fb028ced2e42
Seshan-B <seshan.b@gmail.com>
2024-07-11 18:11:03 +1200

    Massive push

--- odd_even_numbers.py
+++ odd_even_numbers.py
@@ -1,4 +1,10 @@
-
+## Pseudo Code
+# Get user input for a number.
+# Convert the user input to an integer.
+# Assign the integer to a variable.
+# Calculate the modulus of the number by 2.
+# If the result of the modulus operation is greater than or equal to 1, print "This is an odd number."
+# Otherwise, print "This is an even number."
 
 # Get user input
 number_input = int(input("Which number do you want to check? "))
