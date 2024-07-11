cbd37eeb58828fd8586258ed591b331a9a3da2dc
Seshan-B <seshan.b@gmail.com>
2024-07-11 18:18:25 +1200

    Added comments

--- randomised_password_generator.py
+++ randomised_password_generator.py
@@ -14,60 +14,50 @@
 
 import random
 
+# List of letters (both lowercase and uppercase)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-
-# Letters
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-
-# Numbers
+# List of numbers
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-
-# Symbols
+# List of symbols
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
 
-user_letters= int(input("How many letters would you like in your password?\n")) 
+# Get user input for the number of letters, symbols, and numbers
+user_letters = int(input("How many letters would you like in your password?\n"))
+user_symbols = int(input("How many symbols would you like?\n"))
+user_numbers = int(input("How many numbers would you like?\n"))
 
-user_symbols = int(input(f"How many symbols would you like?\n"))
+# Initialize an empty list to hold the password characters
+password_list = []
 
-user_numbers = int(input(f"How many numbers would you like?\n"))
-
-
-# Initialize list
-password_list =  []
-
-
-# Loop letters
+# Loop to add random letters to the password list
 for each_char in range(1, user_letters + 1):
     random_character = random.choice(letters)
-    #print(random_character)
     password_list += random_character
     print(password_list)
 
-
-# For numbers 
+# Loop to add random numbers to the password list
 for each_char in range(1, user_numbers + 1):
     random_character = random.choice(numbers)
-    #print(random_character)
     password_list += random_character
     print(password_list)
-    
-    
-# For symbols
+
+# Loop to add random symbols to the password list
 for each_char in range(1, user_symbols + 1):
     random_character = random.choice(symbols)
-    #print(random_character)
     password_list += random_character
     print(password_list)
-    
-# Generate the password assign to for loop to append it and convert to string
+
+# Initialize an empty string to hold the final generated password
 generated_password = ""
+# Loop to concatenate each character in the password list to the final password string
 for char in password_list:
     generated_password += char
     print(generated_password)
 
-# Output Generated Password
-print(f" Generated Password: {generated_password}")
+# Print the generated password
+print(f"Generated Password: {generated_password}")
